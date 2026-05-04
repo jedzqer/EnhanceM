20ebc3fd0412bf24f95cfea8effd4243ef53c1a1
jedzqer <jedzouquan@gmail.com>
2026-05-04 21:04:47 +0800

    实现经验球2.5倍经验获取效率

---
+++ src/main/java/net/enhancem/mixin/ExperienceMultiplierMixin.java
@@ -0,0 +1,20 @@
+package net.enhancem.mixin;
+
+import net.minecraft.world.entity.ExperienceOrb;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ExperienceOrb.class)
+public class ExperienceMultiplierMixin {
+
+	@Unique
+	private static final double EXPERIENCE_MULTIPLIER = 2.5;
+
+	@Inject(method = "getValue", at = @At("RETURN"), cancellable = true)
+	private void onGetValue(CallbackInfoReturnable<Integer> cir) {
+		cir.setReturnValue((int) (cir.getReturnValue() * EXPERIENCE_MULTIPLIER));
+	}
+}
