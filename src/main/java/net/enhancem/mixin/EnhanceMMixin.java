bf555cabdd3dc36ec8a9ebd5a10c4a8695a04153
jedzqer <jedzouquan@gmail.com>
2026-05-17 15:23:45 +0800

    移除低质量的末影人修改

--- src/main/java/net/enhancem/mixin/EnhanceMMixin.java
+++ src/main/java/net/enhancem/mixin/EnhanceMMixin.java
@@ -1,8 +1,6 @@
 package net.enhancem.mixin;
 
-import net.enhancem.EndermanBlackHoleManager;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.level.ServerLevel;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,13 +10,5 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class EnhanceMMixin {
 	@Inject(at = @At("HEAD"), method = "loadLevel")
 	private void init(CallbackInfo info) {
-	}
-
-	@Inject(method = "tickServer", at = @At("HEAD"))
-	private void enhancem$tickBlackHoles(CallbackInfo ci) {
-		MinecraftServer server = (MinecraftServer) (Object) this;
-		for (ServerLevel level : server.getAllLevels()) {
-			EndermanBlackHoleManager.tick(level);
-		}
 	}
 }
